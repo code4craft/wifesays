6030fe11d7c74d38196f41753e310203977520f7
code4craft <code4crafter@gmail.com>
2013-05-07 19:32:43 +0800

    make startup error more human-friendly

--- src/main/java/us/codecraft/wifesays/me/TCPSocketMonitor.java
+++ src/main/java/us/codecraft/wifesays/me/TCPSocketMonitor.java
@@ -1,11 +1,7 @@
 package us.codecraft.wifesays.me;
 
 import java.io.IOException;
-import java.net.Inet4Address;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.SocketException;
+import java.net.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -14,86 +10,90 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class TCPSocketMonitor extends Thread {
 
-	private Logger log = Logger.getLogger(this.getClass());
+    private Logger log = Logger.getLogger(this.getClass());
 
-	private InetAddress addr;
-	private int port;
-	private ServerSocket serverSocket;
-	@Autowired
-	private HusbandEar husbandEar;
+    private InetAddress addr;
+    private int port;
+    private ServerSocket serverSocket;
+    @Autowired
+    private HusbandEar husbandEar;
 
-	private ExecutorService executorService = Executors.newFixedThreadPool(100);
+    private ExecutorService executorService = Executors.newFixedThreadPool(100);
 
-	public TCPSocketMonitor(String host, int port) throws IOException {
-		super();
-		this.port = port;
-		this.addr = Inet4Address.getByName(host);
-		serverSocket = new ServerSocket(port, 128, addr);
+    public TCPSocketMonitor(String host, int port) {
+        super();
+        this.port = port;
+        try {
+            this.addr = Inet4Address.getByName(host);
+            serverSocket = new ServerSocket(port, 128, addr);
+        } catch (IOException e) {
+            log.error("Start tcp monitor failed, check whether port " + port + " is taken. Maybe an instance is already running", e);
+        }
 
-		this.setDaemon(true);
-	}
+        this.setDaemon(true);
+    }
 
-	@Override
-	public void run() {
+    @Override
+    public void run() {
 
-		log.info("Starting TCP socket monitor on address "
-				+ getAddressAndPort());
+        log.info("Starting TCP socket monitor on address "
+                + getAddressAndPort());
 
-		while (true) {
-			try {
+        while (true) {
+            try {
 
-				final Socket socket = serverSocket.accept();
+                final Socket socket = serverSocket.accept();
 
-				log.debug("TCP connection from "
-						+ socket.getRemoteSocketAddress());
-				if (!socket.getInetAddress().getHostAddress()
-						.equals("127.0.0.1")) {
-					continue;
-				}
-				executorService.execute(new TCPConnection(socket, husbandEar));
+                log.debug("TCP connection from "
+                        + socket.getRemoteSocketAddress());
+                if (!socket.getInetAddress().getHostAddress()
+                        .equals("127.0.0.1")) {
+                    continue;
+                }
+                executorService.execute(new TCPConnection(socket, husbandEar));
 
-			} catch (SocketException e) {
+            } catch (SocketException e) {
 
-				// This is usally thrown on shutdown
-				log.debug("SocketException thrown from TCP socket on address "
-						+ getAddressAndPort() + ", " + e);
-				break;
+                // This is usally thrown on shutdown
+                log.debug("SocketException thrown from TCP socket on address "
+                        + getAddressAndPort() + ", " + e);
+                break;
 
-			} catch (IOException e) {
+            } catch (IOException e) {
 
-				log.info("IOException thrown by TCP socket on address "
-						+ getAddressAndPort() + ", " + e);
-			}
-		}
-		log.info("TCP socket monitor on address " + getAddressAndPort()
-				+ " shutdown");
-	}
+                log.info("IOException thrown by TCP socket on address "
+                        + getAddressAndPort() + ", " + e);
+            }
+        }
+        log.info("TCP socket monitor on address " + getAddressAndPort()
+                + " shutdown");
+    }
 
-	public InetAddress getAddr() {
+    public InetAddress getAddr() {
 
-		return addr;
-	}
+        return addr;
+    }
 
-	public int getPort() {
+    public int getPort() {
 
-		return port;
-	}
+        return port;
+    }
 
-	public ServerSocket getServerSocket() {
+    public ServerSocket getServerSocket() {
 
-		return serverSocket;
-	}
+        return serverSocket;
+    }
 
-	public void closeSocket() throws IOException {
+    public void closeSocket() throws IOException {
 
-		log.info("Closing TCP socket monitor on address " + getAddressAndPort()
-				+ "...");
+        log.info("Closing TCP socket monitor on address " + getAddressAndPort()
+                + "...");
 
-		this.serverSocket.close();
-	}
+        this.serverSocket.close();
+    }
 
-	public String getAddressAndPort() {
+    public String getAddressAndPort() {
 
-		return addr.getHostAddress() + ":" + port;
-	}
+        return addr.getHostAddress() + ":" + port;
+    }
 }
